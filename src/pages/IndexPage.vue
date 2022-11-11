b9a00d18c8b66bf75b51ed918afc7b4c922eda79
Alini Canedo <alini.canedo@gmail.com>
2022-11-11 17:48:29 -0300

    configuring route to home and creating first layout

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,31 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page padding>
+    <div id="nav">
+      <router-link to="/basket">Carrinho (0)</router-link>
+    </div>
+    <div class="q-pa-md row items-start q-gutter-md">
+      <q-card class="my-card">
+        <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg">
+        <q-card-section>
+          <div class="text-h6">Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</div>
+          <p class="price">R$ 109.95</p>
+        </q-card-section>
+
+        <q-separator />
+
+        <q-card-actions align="right">
+          <q-btn flat>Adicionar ao carrinho</q-btn>
+        </q-card-actions>
+      </q-card>
+    </div>
   </q-page>
 </template>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 250px
+</style>
 
 <script>
 import { defineComponent } from 'vue'
